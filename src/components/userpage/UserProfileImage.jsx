2ccec1358b62b4f391e5f08444ca9851c14d8d1d
Ori Dahari <oridahari15@gmail.com>
2025-04-24 16:55:16 +0300

    fix not loading profile picture to run a useEffect hook dependent on user

--- src/components/userpage/UserProfileImage.jsx
+++ src/components/userpage/UserProfileImage.jsx
@@ -1,19 +1,33 @@
 import React, { useEffect, useState } from 'react'
-import LogoIcon from '../icons/LogoIcon'
+import { useAuth } from '@frontegg/react'
 
-const UserProfileImage = ({user}) => {
-  const [src, setSrc] = useState(user.profilePictureUrl || null)
+// Components
+import defaultImage from '../../assets/egg_96x400.png'
+
+
+const UserProfileImage = () => {
+  const { user } = useAuth()
+  const [src, setSrc] = useState(user?.profilePictureUrl)
+
+  const updateImageSource = () => {
+    if (!src) {
+      setSrc(user?.profilePictureUrl)
+    }
+  }
 
   useEffect(() => {
-    setSrc(user.profilePictureUrl || null)
+    updateImageSource()
   }, [user])
-
+  
+  const ProfileImage = () => {
+    return (<img src={src || defaultImage} className='object-contain w-full rounded-full' alt="profile picture" />)
+  }
   return (
-    <div className={`flex h-full min-h-[160px] aspect-square justify-center items-center rounded-full 
+    <div className={`flex h-full min-h-[160px] aspect-square justify-center items-center rounded-full font-beauty
             bg-white border-gray-100 border-4 overflow-hidden shadow-sm shadow-frontegg-dark text-center text-gray-400
             ${src ? '' : 'p-5'}`}
     >
-        {src ? <img src={src} className='object-contain w-full rounded-full' alt="Profile Picture" /> : <LogoIcon />}
+      <ProfileImage />
     </div>
   )
 }
